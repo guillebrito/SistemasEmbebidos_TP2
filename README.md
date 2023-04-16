68511d2b614418f7bdc6ea21ecb0ffc4e38c32e6
Guillermo Nicolas Brito <guillermonbrito@gmail.com>
2023-04-16 14:36:13 -0300

    Implementación del programa

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Trabajo Práctico Número 2
 
--Como se compila
+Para compilar el proyecto se debe ejecutar el comando `make` por consola, luego para correr el programa elecutar el siguiente comando `./build/bin/app.elf`
 
 Autor: Brito, Guillermo Nicolás
 Asignatura: Sistemas Embebidos
---
+++ src/main.c
@@ -0,0 +1,72 @@
+/************************************************************************************************
+Copyright (c) 2023, Guillermo Nicolás Brito <Guillermonbrito@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** \brief Segundo trabajo práctico
+ **
+ ** Full file description
+ **
+ ** \addtogroup tp2 TPN2
+ ** \brief Segundo práctico
+ ** @{ */
+
+/* === Headers files inclusions =============================================================== */
+
+#include "alumno.h"
+#include <stdio.h>
+
+/* === Macros definitions ===================================================================== */
+
+/* === Private data type declarations ========================================================= */
+
+/* === Private variable declarations ========================================================== */
+
+/* === Private function declarations ========================================================== */
+
+/* === Public variable definitions ============================================================ */
+
+/* === Private variable definitions =========================================================== */
+
+/* === Private function implementation ======================================================== */
+
+/* === Public function implementation ========================================================= */
+int main(void)
+{
+    char cadena[128];
+    int resultado;
+    static const struct alumno_s yo = {
+        .apellido = "Brito",
+        .nombre = "Guillermo Nicolás",
+        .documento = 39141889,
+    };
+
+    if (Serializar(&yo, cadena, sizeof(cadena)) >= 0)
+    {
+        printf("%s\n", cadena);
+    }
+    else
+    {
+        printf("Error al serializar\n");
+    }
+    return 0;
+}
+/* === End of documentation =================================================================== */
+
+/** @} End of module definition for doxygen */
